cf47f45c6d3b4f0d0778270bea371545344c1734
Lucio Soares de Oliveira <oliveira.soaers.l@hotmail.com>
2016-05-17 17:37:01 -0300

    ajuste atualizar e remover paciente

--- ClinicaMedicaFramework/src/dao/PacienteDao.java
+++ ClinicaMedicaFramework/src/dao/PacienteDao.java
@@ -2,66 +2,68 @@ package dao;
 
 import dominio.Paciente;
 
-public class PacienteDao extends GenericDao<Paciente>{
+public class PacienteDao extends GenericDao<Paciente> {
 
-    
     public PacienteDao() {
-        super(System.getProperty("user.dir")+ "/src/xmlVeterinaria/paciente.xml");   
+        super(System.getProperty("user.dir") + "/src/xmlVeterinaria/paciente.xml");
         System.out.println(listagem.size());
     }
 
     public boolean existe(Paciente paciente) {
         return false;
     }
-    
+
     public void salvar(Paciente paciente) {
-        paciente.setId(listagem.size()+1);
+        System.err.println(listagem.size() + 1);
+        paciente.setId(listagem.size() + 1);
         listagem.add(paciente);
         super.salvar();
         System.err.println(listagem.size());
     }
 
     public void atualizar(Paciente paciente) {
-        listagem.stream().filter((aux) -> (aux.getId() == paciente.getId())).forEach((aux) -> {
-            aux = paciente;
-        });
-       super.atualizar();
+        int i = 0;
+        Paciente alterar = null;
+        for (Paciente aux : listagem) {
+            if (aux.getId() == paciente.getId()) {
+                aux = paciente;                
+            }
+            i++;
+        }
+        listagem.remove(alterar);
+        listagem.add(i,paciente);
+        super.atualizar();
     }
 
     public void remover(Paciente paciente) {
-        System.err.println(paciente.getId());
-        listagem.stream().filter((aux) -> (aux.getId() == paciente.getId())).forEach((aux) -> {
-            System.err.println(paciente.getId()+" = "+aux.getId());
-            listagem.remove(aux);
-        });
+        Paciente remover = null;
+        for (Paciente aux : listagem) {
+            if (aux.getId() == paciente.getId()) {
+                remover = aux;
+            }
+        }
+        listagem.remove(remover);
         super.remover();
     }
 
-    
-    public Paciente buscarID(int id){
+    public Paciente buscarID(int id) {
         Paciente paciente = null;
-        for(Paciente aux : listagem){
-            if(aux.getId() == id)
+        for (Paciente aux : listagem) {
+            if (aux.getId() == id) {
                 paciente = aux;
+            }
         }
         return paciente;
     }
-    
-    public Paciente buscarNome(String nome){
-       Paciente paciente = null;
-       for(Paciente aux : listagem){
-           if(aux.getNome() == nome)
-               paciente = aux;
-       }
-           return paciente;
-    }
-       
-      
-      
-    
-    
 
-    
-    
+    public Paciente buscarNome(String nome) {
+        Paciente paciente = null;
+        for (Paciente aux : listagem) {
+            if (aux.getNome() == nome) {
+                paciente = aux;
+            }
+        }
+        return paciente;
+    }
 
 }
