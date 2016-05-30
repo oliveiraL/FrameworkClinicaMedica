02602793e04a6b23d2c55a1daaa662dccd98b16d
victorgustavo <victor_gustavo_2008@hotmail.com>
2016-05-30 12:12:05 -0300

    GerenciarEspecialista e GerenciarEspecialida

---
+++ ClinicaMedicaFramework/src/clinicaEstetica/dominio/DoctorEstetica.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clinicaEstetica.dominio;
+
+import dominio.Especialidade;
+import dominio.Especialista;
+import validacoes.ValidacaoException;
+
+/**
+ *
+ * @author Victor
+ */
+public class DoctorEstetica extends Especialista{
+     private String crmv;
+     
+     public DoctorEstetica(String crmv, Especialidade especialidade, String nome, String CPF){
+         super(especialidade, nome, CPF);
+        this.crmv = crmv;
+     }
+     
+    @Override
+    public void validarEspecialista() throws ValidacaoException {
+        if(getCrmv() == null || getCPF() == null || getCPF().isEmpty() || getCrmv().isEmpty())
+            throw new ValidacaoException("Erro ao validar Veterinario.");
+    }
+    
+    public String getCrmv() {
+        return crmv;
+    }
+
+    public void setCrmv(String crmv) {
+        this.crmv = crmv;
+    }
+    
+}
